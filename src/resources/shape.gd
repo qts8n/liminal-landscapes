acda76db7c262559452ec6c90b7fa01875cefc32
E F <efisher9680@gmail.com>
2024-08-29 00:27:25 +0400

    feat: Noise filter layers in shape resource

--- src/resources/shape.gd
+++ src/resources/shape.gd
@@ -7,3 +7,23 @@ class_name Shape
 	set(new_radius):
 		radius = new_radius
 		changed.emit()
+
+@export var first_layer_mask: bool = true:
+	set(new_first_layer_mask):
+		first_layer_mask = new_first_layer_mask
+		changed.emit()
+
+@export var noise_layers: Array[UniformNoise] = []:
+	set(new_noise_layers):
+		for noise in noise_layers:
+			if noise != null and noise.changed.is_connected(_update_shape):
+				noise.changed.disconnect(_update_shape)
+		noise_layers = new_noise_layers
+		for noise in noise_layers:
+			if noise != null:
+				noise.changed.connect(_update_shape)
+		_update_shape()
+
+
+func _update_shape() -> void:
+	changed.emit()
